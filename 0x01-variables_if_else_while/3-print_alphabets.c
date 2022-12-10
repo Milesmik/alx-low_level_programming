f20d5e6b80ead8ae22d2f54cf98e2c060fd98ae2
milesmik <timileyin08@gmail.com>
2022-12-09 21:21:15 -0800

    If

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mike;
+	
+	for (mike = 'a'; mike <= 'z'; mike++)
+	{
+		putchar(mike);
+	}
+	for (mike = 'A'; mike <= 'Z'; mike++)
+	{
+		putchar(mike);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
