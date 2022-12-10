8e76ab2b03196b2abb01a26a28b78fe9a9ee111a
milesmik <timileyin08@gmail.com>
2022-12-09 20:32:47 -0800

    If

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		char alphabet
+		putchar("Lowercase alphabets:\n");
+		/*loop statement, that will check the condition 
+		 * and print the alphabets from 'a' to 'z' */
+		while(alphabet<='z')
+		{
+		putchar("%c ",alphabet);
+		/* increasing the value by 1 */
+												alphabet++;
+		}
+												return 0;
+}
