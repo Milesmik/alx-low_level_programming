d2e9469c072cd816de3e41ca7c6d2eef7e8f9884
milesmik <timileyin08@gmail.com>
2022-12-09 21:33:22 -0800

    If

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mike
+		for (mike = 'a'; mike <= 'z'; mike ++; mike != 'q'; mike != 'e')
+		{
+		putchar(mike);
+		}
+		{
+		putchar('\n');
+		}
+	return (0);
+}
