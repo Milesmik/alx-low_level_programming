d57f1b351266b0164781e56baaf623850fbc8b75
milesmik <timileyin08@gmail.com>
2022-12-12 00:55:30 -0800

    If

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num
+	while (num = '1'; num <= '9')
+	{
+		printf("%d \n", num);
+		(num++);
+	}
+	printf("\n");
+	return (0);
+}
+
