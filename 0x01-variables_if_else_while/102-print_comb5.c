b5a00cd40776f762e1295e516e45acd1c8771d41
milesmik <timileyin08@gmail.com>
2022-12-12 19:08:59 -0800

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,13 +14,13 @@ int main(void)
 
 	for (digit_1 = 0; digit_1 <= 98; digit_1++)
 	{
-		for (digit_2 = 0; digit_2 <= 99; digit_2++)
+		for (digit_2 = 1; digit_2 <= 99; digit_2++)
 		{
 			putchar((digit_1 / 10) + '0');
 			putchar((digit_1 % 10) + '0');
 			putchar(' ');
 			putchar((digit_2 / 10) + '0');
-			putchar((digit_2 % 10) + '1');
+			putchar((digit_2 % 10) + '0');
 
 			if (digit_1 < 98 && digit_2 < 99)
 				continue;
