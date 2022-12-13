f7b0e3dcd3e52ddb775b6bb9734a37d256be68d6
milesmik <timileyin08@gmail.com>
2022-12-12 17:11:06 -0800

    really

---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: This is a test fiel to evaluate the function (())
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	while (num = 0; num <=9; num++)
+	{
+		putchar(num);
+			if (num == 9)
+			break;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
