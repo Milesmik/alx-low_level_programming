7d2c0bffbc92c4b3029c6379480d59d0952036b1
milesmik <timileyin08@gmail.com>
2022-12-13 16:51:48 -0800

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - Entry point
+ * Description: The prototypes of all your functions and the prototype of the function.
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c);
