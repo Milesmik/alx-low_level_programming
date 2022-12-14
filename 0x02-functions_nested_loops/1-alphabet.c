6b3813b9182ef19d6f0e4569a6d2655b9ca9daa0
milesmik <timileyin08@gmail.com>
2022-12-13 19:26:53 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Deccription: function prints alphabetd in lowercasee.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+		_putchar('\n');
+	
+	return (0);
+}
