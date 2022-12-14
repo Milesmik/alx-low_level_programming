2ce8712dc0d5d7a60ff6bbafd1a4cfea2fce16b9
milesmik <timileyin08@gmail.com>
2022-12-13 20:20:48 -0800

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Deccription: function prints alphabets lowercasee.
+ * print_alphabet_x10 - Entry point
+ * Deccription: function prints alphabets lowercasee 10 times..
  * Return: Always 0 (Success)
  */
 
@@ -11,11 +11,11 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char x;
 
-	while (count++ <=9)
+	while (count++ <= 9)
 	{
 		for (x = 'a'; x <= 'z'; x++)
 			_putchar(x);
-	
+
 		_putchar('\n');
 	}
 }
