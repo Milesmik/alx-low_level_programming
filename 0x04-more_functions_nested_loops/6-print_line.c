fb68c7baf3638ecceb228863e16e5b27bbcdba83
milesmik <timileyin08@gmail.com>
2022-12-15 21:00:59 -0800

    nested

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- *  * print_line - Draws a straight line using the character _.
- *   * @n: The number of _ characters to be printed.
- *    */
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+
 void print_line(int n)
 {
 	int x;
