40dce8c76a993bf1c37e20af71729bfbfb042b2b
milesmik <timileyin08@gmail.com>
2022-12-15 20:23:22 -0800

    nested

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,13 +4,10 @@
  * int mul - multiplies two integers
  * @a: Integer 1
  * @b: integer 2
- * Return: Always 0 (Success)
+ * Return: The result of the multiplication
  */
 
 int mul(int a, int b)
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-	
-	return (0);
+	return (a * b);
 }
