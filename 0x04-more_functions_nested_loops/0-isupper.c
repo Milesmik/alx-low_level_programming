032eb1a1933e0f9823bf2ef6091a4a932f882632
milesmik <timileyin08@gmail.com>
2022-12-15 19:29:47 -0800

    neste functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
+ * _isupper - check the code for capital letter.
+ * @c: decleared variable for the alphabets.
  * Return: Always 0.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	
+
 		return (1);
 
 	else
