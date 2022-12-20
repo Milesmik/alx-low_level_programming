0c7b480a611597e586099b3bf5b8156a8dc7ec1d
milesmik <timileyin08@gmail.com>
2022-12-19 21:06:24 -0800

    pointer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,6 +9,7 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
 	while (*(s + count) != '\0')
 	{
 	if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
@@ -17,9 +18,9 @@ int _atoi(char *s)
 		pn *= -1;
 	if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 	{
-	if (size > 0)
-																	m *= 10;
-	size++;
+		if (size > 0)
+			m *= 10;
+		size++;
 	}
 	count++;
 	}
