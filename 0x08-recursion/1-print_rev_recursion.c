86eb81b8fc58349d24525b9d75f425ee942f473d
michael <timileyin08@gmail.com>
2023-08-23 17:54:43 +0100

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - prints reverse recursion
+ * _print_rev_recursion - prints reverse recursion
  * @s: character to be printed
  * Return: returns void
  */
