e7bd30ef7596d2b5da0e4fcb2547e74a852ae4bb
michael <timileyin08@gmail.com>
2023-08-23 17:47:30 +0100

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAINN_H_
 
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-void _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -11,9 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _sqrt(int n, int x);
-int is_prime(int n, int start);
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
 
 #endif
