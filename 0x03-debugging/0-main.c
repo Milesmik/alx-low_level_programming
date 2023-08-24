9cfded38db9fc40f8f56f50a32091229f33f7806
michael <timileyin08@gmail.com>
2023-08-24 09:28:49 +0100

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point, test function that prints
+ * if integer is negative or positive
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
