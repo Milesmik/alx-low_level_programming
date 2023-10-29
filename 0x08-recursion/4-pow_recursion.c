03cb3d66cfb5f75c939c69c9bfddcd286378300b
michael <timileyin08@gmail.com>
2023-10-29 20:19:12 +0100

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,6 +14,10 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);  
+	else if (x < 0)
+		return (-1);
+	else if (x => 0)
+		return (1);
 	else
 		return (_pow_recursion(x, y));
 }
