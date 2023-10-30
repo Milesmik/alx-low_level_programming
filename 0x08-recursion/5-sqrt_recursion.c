b3ade317a3d1fe120862bec20882434af70fc14a
michael <timileyin08@gmail.com>
2023-10-30 07:16:19 +0100

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,45 @@
 #include <main.h>
 
 /**
- * power_operation - returns the natural square root of a number.
- * @n: input number.
- * @c: iterator.
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @low: low band of n.
+ * @high: high bank=d of n.
+ * @mid: mid of n.
+ * @n: the nth number.
+ * @square: the square of n.
  * Return: square root or -1.
  */
-int power_operation(int n, int c)
-{
-	if (c % (n / c) == 0)
-	{
-		if (c * (n / c) == n)
-			return (c);
-		else
-			return (-1);
-	}
-	return (0 + power_operation(n, c + 1));
-}
 
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- * Return: natural square root.
- */
-
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n, int low, int high)
 {
 	if (n < 0)
+	{
 		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	return (power_operation(n, 2));
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else if (low <= high)
+	{
+		int mid = (low + high) / 2;
+		int square = mid * mid;
+
+		if (square == n)
+		{
+			return (mid);
+		}
+		else if (square < n)
+		{
+			return (_sqrt_recursion(n, mid + 1, high));
+		}
+		else
+		{
+			return (_sqrt_recursion(n, low - 1));
+		}
+	}
+	else
+	{
+		return (high);
+	}
 }
