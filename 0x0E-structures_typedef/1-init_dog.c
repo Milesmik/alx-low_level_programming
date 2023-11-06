59451d1a9aacf74c0549df1d75202cd520c930f2
michael <timileyin08@gmail.com>
2023-11-06 14:41:15 +0100

    01

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * struct dog - struct that stores the information of a dog
+ * 
+ * *d: struct dog.
+ * @name: name of the dog.
+ * @age: dogs age.
+ * @owner: dog's owner.
+ *
+ * Return: no return.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
